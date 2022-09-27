1004d7f278855aac43769ce68c4043ee1a26a990
SERGIO MANUEL partida gutierrez <sheko3857@gmail.comgit config --global user.name sergio>
2022-09-27 13:16:47 -0700

    created action de getByName

--- cell-store/src/redux/actions.js
+++ cell-store/src/redux/actions.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 export const GET_ALL_PRODUCTS = 'GET_ALL_PRODUCTS'
+export const GET_PRODUCT_BY_NAME = "GET_PRODUCT_BY_NAME"
 
 axios.defaults.baseURL = 'http://localhost:3001'
 
@@ -18,6 +19,25 @@ export const getAllProducts = () => {
         }
     }
 }
+
+export function getProductByName(name){
+    return async function (dispatch){
+
+     try {
+      const product = await axios.get(`/products?name=${name}`)
+      return dispatch({
+        type:GET_PRODUCT_BY_NAME,
+        payload:product.data
+     })
+     } catch (error) {
+        console.log(error)
+     }
+     
+   }
+
+}
+   
+  
 
 
 
