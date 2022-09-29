461763d8c41f124561713db99df8e0e4cf0fd1f1
tinoyao <dicienbre013@gmail.com>
2022-09-29 14:45:25 -0300

    algo

--- cell-store/src/App.jsx
+++ cell-store/src/App.jsx
@@ -1,14 +1,19 @@
 /* import { useState } from 'react'
 import logo from './logo.svg' */
-/* import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; */
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
 import Home from './page/Home/Home';
-/* import Detail from './components/cards-products/Detail' */
+import Detail from './components/cards-products/Detail'
 function App() {
   
 
   return (
-    < Home />
+    <Router>
+      <Routes>
+        <Route path='/:id' element={<Detail/>} />
+        <Route path='/' element={<Home/>} />
+      </Routes>
+    </Router>
     
   )
 }
