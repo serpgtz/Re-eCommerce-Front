6d4a27e7552f5b7ae2ce8664645c3cc71c1a307f
EmanFun <emanuelfun.95@gmail.com>
2022-09-30 01:38:19 -0300

    feat(Main / Routes ): add routes

--- cell-store/src/main.jsx
+++ cell-store/src/main.jsx
@@ -7,6 +7,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./page/Home/Home";
 import ProductForm from "./page/Form/ProductForm";
 import Detail from "./components/cards-products/Detail";
+
+
+import InterForm from './page/Form/InterForm';
+import CategoryForm from './page/Form/CategoryForm'
+
+
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
@@ -15,6 +22,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           <Route path="/" element={<Home />} />
           <Route path="/detail/:id" element={<Detail />} />
           <Route path="/newproduct" element={<ProductForm />} />
+          <Route path="/interForm/:id" element={<InterForm/>} />
+          <Route path="/categoryForm/:id" element={<CategoryForm/>}/>
         </Routes>
       </BrowserRouter>
     </Provider>
