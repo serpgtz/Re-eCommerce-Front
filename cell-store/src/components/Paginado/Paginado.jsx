faea764a8d7f66a45ba2e29dd13db19e18245b84
sergio <sheko3857@gmail.com>
2022-10-01 17:22:16 -0700

    se modifico limite

--- cell-store/src/components/Paginado/Paginado.jsx
+++ cell-store/src/components/Paginado/Paginado.jsx
@@ -5,7 +5,7 @@ import s from "../Paginado/Paginado.module.css"
 
 
 export default function(){
-    let limit = 15
+    let limit = 8
     const productsTotal = useSelector(state=>state.product.products)
 
     const dispatch = useDispatch()
