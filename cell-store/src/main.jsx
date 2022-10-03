3331a62b2a4a7692b5de23835ec51b281fd20648
Chaos <antonioscutari2@hotmail.com>
2022-10-03 18:51:47 -0300

    añadido admin Dashboard

--- cell-store/src/main.jsx
+++ cell-store/src/main.jsx
@@ -16,34 +16,32 @@ import Historia from "./page/historia/Historia";
 import Profile from "./page/Profile/Profile";
 import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/footer/Footer";
-import {ChakraProvider} from '@chakra-ui/react'
-import theme from './theme'
-
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme";
+import Dashboard from "./page/Dashboard/Dashboard";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-     <ChakraProvider theme={theme} resetCSS={false}>
-      <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/detail/:id" element={<Detail />} />
-          <Route path="/newproduct" element={<ProductForm />} />
-          <Route path="/interForm/:id" element={<InterForm />} />
-          <Route path="/categoryForm/:id" element={<CategoryForm />} />
-          <Route path="/about" element={<About />} />
-          
-          <Route path="/account/login" element={<Auth />} />
-          <Route path="/account/register" element={<Register />} />
-          
-          <Route path="/account/profile" element={<Profile />} />
-          <Route path="/historia" element={<Historia />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      <ChakraProvider theme={theme} resetCSS={false}>
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/detail/:id" element={<Detail />} />
+            <Route path="/newproduct" element={<ProductForm />} />
+            <Route path="/interForm/:id" element={<InterForm />} />
+            <Route path="/categoryForm/:id" element={<CategoryForm />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/account/login" element={<Auth />} />
+            <Route path="/account/register" element={<Register />} />
+            <Route path="/adminDashboard" element={<Dashboard />} />
+            <Route path="/account/profile" element={<Profile />} />
+            <Route path="/historia" element={<Historia />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
       </ChakraProvider>
     </Provider>
-    
   </React.StrictMode>
 );
