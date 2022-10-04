ee37befffae79eb9f6a9cdd0240afb3e48dcfce1
gcarreiras <gcarreiras86@gmail.com>
2022-10-04 10:13:29 -0300

    cellstoreh1 to main page

--- cell-store/src/components/NavBar/NavBar.jsx
+++ cell-store/src/components/NavBar/NavBar.jsx
@@ -4,21 +4,31 @@ import { Link, useNavigate } from "react-router-dom";
 import { userLogOut } from "../../redux/actions/userActions";
 import styles from "../NavBar/NavBar.module.css";
 import SearchBar from "../searchBar/searchBar";
+import {changePage, getAllProducts} from "../../redux/actions/productActions";
+
+
+
+
 
 export const NavBar = () => {
   const user = useSelector((state) => state.user.user);
   const dispatch = useDispatch();
   const navigation = useNavigate();
- 
+  
+  function handleClick(e) {
+    dispatch(getAllProducts())
+    dispatch(changePage(1))
+  }
 
   const handleLogOut = () => {
     localStorage.removeItem("token");
     dispatch(userLogOut());
     navigation(-1);
+    
   };
   return (
     <nav className={styles.navBar}>
-      <h1 className={styles.header}>CELL STORE</h1>
+      <Link  to='/' className={styles.header} onClick={(e) => {handleClick(e);}} > <h1>CELL STORE</h1></Link> 
       {user.admin === true ? (
         <Link className={styles.link} to="/newproduct">
           <button className={styles.navBtn}> Crear Producto </button>
