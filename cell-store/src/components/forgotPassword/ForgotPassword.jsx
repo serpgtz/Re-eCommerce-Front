ffd83c1d7222320a396d4d6a41dcbbf84d44cc86
Tomas Oyarzun <xilent312@gmail.com>
2022-10-11 23:13:58 -0300

    fix css olvide password

--- cell-store/src/components/forgotPassword/ForgotPassword.jsx
+++ cell-store/src/components/forgotPassword/ForgotPassword.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { forgotPasswordEmail } from '../../redux/actions/userActions'
 import style from './ForgotPassword.module.css'
+import alert_icon from '../../image/alerta.png'
 const forgotPassword = ({active, setActive}) => {
     
      const dispatch = useDispatch()
@@ -32,12 +33,17 @@ const forgotPassword = ({active, setActive}) => {
      
      <h2>Escriba su email</h2>
      <button className={style.button} onClick={()=> setActive(!active)}>X</button>
-     {response?.msg && <div style={{background: response.error ? 'red' : 'green'}} className={style.div_msg}>{response.msg}</div>}
+     
      <form onSubmit={handleOnSubmit} className={style.form}>
          <input type='text' name='email' onChange={handleOnChange} value={input.email}></input>
          <input type='submit'></input>
      </form>
    </div>
+   {response?.msg && <div style={{background: response.error ? 'rgb(223, 134, 134)' : 'rgb(141, 214, 141)'}} className={style.div_msg}>
+       {response.error === false && <img src={alert_icon} alt='alert'></img>}
+        <p>{response.msg}</p>
+        
+        </div>}
  </div>
   )
 }
