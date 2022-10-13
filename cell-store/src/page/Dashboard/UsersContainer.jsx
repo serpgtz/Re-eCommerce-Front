cb9135d5a75f1b74840031dd19dcb273e63878d6
Chaos <antonioscutari2@hotmail.com>
2022-10-12 23:27:24 -0300

    Refactorizando toda la lista de usuarios

--- cell-store/src/page/Dashboard/UsersContainer.jsx
+++ cell-store/src/page/Dashboard/UsersContainer.jsx
@@ -1,40 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { cardUserContainer, cardUserAdmin, p } from "./Dashboard.module.css";
+import {
+  cardUserContainer,
+  tableUserAdmin,
+  th,
+  td,
+  tableBoxTrue,
+  tableBoxFalse,
+  tdActions,
+} from "./Dashboard.module.css";
 import Delete from "./Delete";
 
 const UsersContainer = ({ users, slideIndex, slideIndex2 }) => {
+  const [modbox, setModBox] = useState(false);
+  const handleModBox = () => {
+    setModBox(!modbox);
+  };
   return (
     <div className={cardUserContainer}>
-      {users?.length
-        ? users?.slice(slideIndex, slideIndex2).map((usuario) => (
-            <Link
-              style={{ textDecoration: "none" }}
-              to={`/user/${usuario._id}`}
-            >
-              {usuario.admin === true ? null : <Delete id={usuario._id} />}
-
-              <div
-                className={cardUserAdmin}
-                style={
-                  usuario.admin === true
-                    ? {
-                        background:
-                          "linear-gradient(190deg, #9a009a 0%, #fff 91%)",
-                      }
-                    : null
-                }
-              >
-                <p className={p}>ID:{usuario._id}</p>
-                <p className={p}>Nombre: {usuario.username}</p>
-                <p className={p}>e-mail: {usuario.email}</p>
-                <p className={p}>
-                  Admin: {usuario.admin === true ? "Sí soy" : "No, no soy"}
-                </p>
-              </div>
-            </Link>
-          ))
-        : null}
+      <table className={tableUserAdmin}>
+        <tr>
+          <th className={th}>NOMBRE</th>
+          <th className={th}>E-MAIL</th>
+          <th className={th}>ACTIONS</th>
+        </tr>
+        {users?.length
+          ? users?.slice(slideIndex, slideIndex2).map((usuario) => (
+              <tr key={usuario.id}>
+                <td className={td}>{usuario.username}</td>
+                <td className={td}>{usuario.email}</td>
+                <td className={tdActions}>
+                  {usuario.admin === true ? (
+                    <div key={usuario.id + "table"} className={tableBoxTrue}>
+                      -No autorizadas-
+                    </div>
+                  ) : (
+                    <div
+                      key={usuario.id + "editable"}
+                      className={tableBoxFalse}
+                      onClick={handleModBox}
+                    >
+                      Editar
+                    </div>
+                  )}
+                </td>
+              </tr>
+            ))
+          : null}
+        {modbox === true ? (
+          <>
+            <dialog open>
+              <p>Detalles de usuario</p>
+              {users?.slice(slideIndex, slideIndex2).map((usuario) => (
+                <>
+                  <p>{usuario.id}</p>
+                  <p>{usuario.username}</p>
+                  <p>{usuario.email}</p>
+                  <button>Coronar como admin</button>
+                  <Delete />
+                </>
+              ))}
+            </dialog>
+          </>
+        ) : (
+          ""
+        )}
+      </table>
     </div>
   );
 };
