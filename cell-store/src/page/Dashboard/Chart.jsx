d8c43c1db033b1227d9887ff2dec70a0d1efd9a9
Chaos <antonioscutari2@hotmail.com>
2022-10-18 09:05:27 -0300

    Correcciones para aprobar

--- cell-store/src/page/Dashboard/Chart.jsx
+++ cell-store/src/page/Dashboard/Chart.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import { useSelector } from "react-redux";
 
 import s from "./Chart.module.css";
@@ -11,55 +11,81 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
-
-
 const Chart = ({ aspect, title }) => {
   const { orders, spent } = useSelector((state) => state.orders.allOrders);
   const gananciasTotales = spent;
-  const hoy = new Date().toISOString().slice(0, 10);
-  const mes = new Date().toISOString().slice(0, 7);
 
-  const todayOrders = orders?.filter(
-    (t) => t.date?.slice(0, 10) === hoy
-  );
-  const gananciasSemanaAnterior = () => {
-    const fechaAReemplazar = hoy.slice(8, 10);
-    const semanaAtras = hoy.replace(fechaAReemplazar, hoy.slice(8, 10) - 7);
-    const ganancias = orders?.filter(
-      (t) => t.date?.slice(0, 10) > semanaAtras
-    );
-    console.log("ORDENES DE LA SEMANA ANTERIOR", ganancias);
-    return ganancias.map((p) => p.totalPrice).reduce((a, b) => a + b, 0);
-  };
+  //Funciones
 
-  const ordersDelMes = orders?.filter(
-    (t) => t.date?.slice(0, 7) === mes
-  );
-
-  const gananciasDelDia = todayOrders
-    .map((p) => p.totalPrice)
-    .reduce((a, b) => a + b, 0);
-
-  const gananciasDelMes = ordersDelMes
-    .map((p) => p.totalPrice)
-    .reduce((a, b) => a + b, 0);
-
-  const mes2 = () => {
-    const mesAReemplazar = mes.slice(5, 7);
-    const dosMesesAtrás = mes.replace(mesAReemplazar, mesAReemplazar - 2);
-    const gananciasDosMeses = orders?.filter(
-        (t) => t.date?.slice(0, 7) < dosMesesAtrás
-      );
-      return gananciasDosMeses.map((p) => p.totalPrice).reduce((a, b) => a + b, 0);
+  //Función para formatear fechas
+  function formato(string) {
+    if (string.length === 6) {
+      return string.slice(0, 5) + "0" + string.slice(5, 6);
+    }
+    if (string.length === 7) {
+      return string.slice(0, 5) + "" + string.slice(5, 7);
+    }
   }
 
+  //Función para sustituir meses
+  const mesAReemplazar = (month) => {
+    if (month.slice(5, 7) <= 10) return month.slice(5, 7);
+
+    if (month.slice(6, 7) >= 9) return month.slice(6, 7);
+  };
+  //Le pasás cualquiera de las órdenes y te da su ganancia
+  const gananciasPorFiltro = (order) => {
+    return order?.map((p) => p.totalPrice).reduce((a, b) => a + b, 0);
+  };
+
+  //Fechas
+  const hoy = new Date().toISOString().slice(0, 10);
+  const mes = hoy.slice(0, 7);
+  const mesAnterior = formato(
+    mes.replace(mesAReemplazar(mes), mesAReemplazar(mes) - 1)
+  );
+
+  const todayOrders = orders?.filter((t) => t.date?.slice(0, 10) === hoy);
+
+  const gananciasSemanaAnterior = () => {
+    const fechaAReemplazar = hoy.slice(8, 10);
+    const semanaAtras = hoy.replace(fechaAReemplazar, fechaAReemplazar - 7);
+    const ganancias = orders?.filter((t) => t.date?.slice(0, 10) > semanaAtras);
+    console.log("ORDENES DE LA SEMANA ANTERIOR", ganancias);
+    return gananciasPorFiltro(ganancias);
+  };
+
+  const ordersDelMes = orders?.filter((t) => t.date?.slice(0, 7) === mes);
+
+  const gananciasDelDia = gananciasPorFiltro(todayOrders);
+
+  const gananciasDelMes = gananciasPorFiltro(ordersDelMes);
+
+  const mes2 = () => {
+    const dosMesesAtrás = formato(
+      mesAnterior?.replace(
+        mesAReemplazar(mesAnterior),
+        mesAReemplazar(mesAnterior) - 1
+      )
+    );
+
+    const gananciasDosMeses = orders?.filter(
+      (t) => t.date?.slice(0, 7) < dosMesesAtrás
+    );
+    return gananciasPorFiltro(gananciasDosMeses);
+  };
+
   const ganancias3 = () => {
-    const mesAReemplazar = mes.slice(5, 7);
-    const tresMesesAtras = mes.replace(mesAReemplazar, mes.slice(5, 7) - 3);
+    const tresMesesAtras = formato(
+      mesAnterior?.replace(
+        mesAReemplazar(mesAnterior),
+        mesAReemplazar(mesAnterior) - 2
+      )
+    );
     const ganancias = orders?.filter(
       (t) => t.date?.slice(0, 7) < tresMesesAtras
     );
-    return ganancias.map((p) => p.totalPrice).reduce((a, b) => a + b, 0);
+    return gananciasPorFiltro(ganancias);
   };
 
   /* const { lastSalesMonth, beforeLastMonth, lastThreeMonth } = useSelector((state) => state.dashboard); */
@@ -68,8 +94,6 @@ const Chart = ({ aspect, title }) => {
     { name: "Sep", Total: `${mes2()}` },
     { name: "Oct", Total: `${gananciasDelMes}` },
   ];
-
-
 
   return (
     <div className={s.chart}>
