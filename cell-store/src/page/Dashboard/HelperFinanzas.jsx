d877902b56ab01dca547dafc82531c146d899e5b
Chaos <antonioscutari2@hotmail.com>
2022-10-18 04:56:46 -0300

    helper v2

--- cell-store/src/page/Dashboard/HelperFinanzas.jsx
+++ cell-store/src/page/Dashboard/HelperFinanzas.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 const { allOrders } = useSelector((state) => state.orders);
-const totalOrders = allOrders?.orders?.length;
+const orders = orders;
+const totalOrders = orders?.length;
 
 const gananciasTotales = allOrders?.spent;
 //Fechas
@@ -14,22 +15,18 @@ const tresMesesAtrás = mes.replace(mesAReemplazar, mesAReemplazar - 3);
 const fechaAReemplazar = hoy.slice(8, 10);
 const semanaAtrás = hoy.replace(fechaAReemplazar, fechaAReemplazar - 7);
 //Órdenes
-const todayOrders = allOrders?.orders?.filter(
-  (t) => t.date?.slice(0, 10) === hoy
-);
-const ordersSemanaAnterior = allOrders?.orders?.filter(
+const todayOrders = orders?.filter((t) => t.date?.slice(0, 10) === hoy);
+const ordersSemanaAnterior = orders?.filter(
   (t) => t.date?.slice(0, 10) > semanaAtrás
 );
-const ordersDelMes = allOrders?.orders?.filter(
-  (t) => t.date?.slice(0, 7) === mes
-);
-const ordersDelMesAnterior = allOrders?.orders?.filter(
+const ordersDelMes = orders?.filter((t) => t.date?.slice(0, 7) === mes);
+const ordersDelMesAnterior = orders?.filter(
   (t) => t.date?.slice(0, 7) === mesAnterior
 );
-const ordenesTresMeses = allOrders?.orders?.filter(
+const ordenesTresMeses = orders?.filter(
   (t) => t.date?.slice(0, 7) < tresMesesAtrás
 );
 //Le pasás cualquiera de las órdenes
-const gananciasPorFiltro = (orders) => {
-  return orders.map((p) => p.totalPrice).reduce((a, b) => a + b, 0);
+const gananciasPorFiltro = (order) => {
+  return order.map((p) => p.totalPrice).reduce((a, b) => a + b, 0);
 };
