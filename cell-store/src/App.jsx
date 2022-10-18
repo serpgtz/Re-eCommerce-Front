90fb26768d380af00a8873f2fe7639a338e2631c
Chaos <antonioscutari2@hotmail.com>
2022-10-17 21:59:05 -0300

    Conexión con Settings

--- cell-store/src/App.jsx
+++ cell-store/src/App.jsx
@@ -1,4 +1,3 @@
-
 import { useDispatch, useSelector } from "react-redux";
 import { json, Route, Routes } from "react-router-dom";
 import "./App.css";
@@ -26,22 +25,21 @@ import Cart from "./components/Cart/Cart";
 import NotFound from "./components/NotFound/NotFound";
 import { ProtectedRoute } from "./Protected/ProtectedRoute";
 import ChangePassword_forgot from "./page/changePassowrd_forgot/ChangePassword_forgot";
-import UsersList from "./page/Dashboard/UsersList"
+import UsersList from "./page/Dashboard/UsersList";
 import Sidebar from "./page/Dashboard/Sidebar";
 import AdminDashboard from "./page/Dashboard/AdminDashboar";
 import VentasTotales from "./page/Dashboard/VentasTotales";
 import ProductList from "./page/Dashboard/ProductList";
 import OrdersList from "./page/Dashboard/OrdersList";
 import ReviewsList from "./page/Dashboard/ReviewsList";
+import Settings from "./page/Profile/Settings";
 
 /* import Detail from './components/cards-products/Detail' */
 
-
-
 function App() {
-  const user_redux = useSelector(state => state.user.user)
-  const user = JSON.parse(localStorage.getItem('user'))
-  console.log('USER:', user)
+  const user_redux = useSelector((state) => state.user.user);
+  const user = JSON.parse(localStorage.getItem("user"));
+  console.log("USER:", user);
   const dispatch = useDispatch();
   const page = useSelector((state) => state.product.page);
   useEffect(() => {
@@ -62,18 +60,28 @@ function App() {
         {/* */}
         <Route path="/confirm/:id" element={<ConfirmedEmail />} />
         {/* */}
-        <Route path="/changePassword/:id" element={<ChangePassword_forgot/>} />
+        <Route path="/changePassword/:id" element={<ChangePassword_forgot />} />
         <Route path="/historia" element={<Historia />} />
 
-        <Route path="/account/profile" element={
-          <ProtectedRoute isAllowed={user} redirectTo={'/account/login'}>
-
-            <Profile />
-          </ProtectedRoute>} />
+        <Route
+          path="/account/profile"
+          element={
+            <ProtectedRoute isAllowed={user} redirectTo={"/account/login"}>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/account/profile/:userId/usersettings"
+          element={
+            <ProtectedRoute isAllowed={user} redirectTo={"/account/login"}>
+              <Settings />
+            </ProtectedRoute>
+          }
+        />
         {/*autenticado */}
 
         <Route element={<ProtectedRoute isAllowed={user?.admin} />}>
-
           <Route path="/newproduct" element={<ProductForm />} />
           {/*autenticado y administrador*/}
           <Route path="/interForm/:id" element={<InterForm />} />
@@ -84,24 +92,22 @@ function App() {
           {/*autenticado y administrador*/}
           <Route path="/adminDashboard/sidebar" element={<Sidebar />} />
           {/*autenticado y administrador*/}
-          <Route path="/admin/userslist" element={< UsersList />} />
+          <Route path="/admin/userslist" element={<UsersList />} />
           {/*autenticado y administrador*/}
-          <Route path="/admin" element={< AdminDashboard />} />
+          <Route path="/admin" element={<AdminDashboard />} />
           {/*autenticado y administrador*/}
-          <Route path="/admin/ventastotales" element={< VentasTotales />} />
+          <Route path="/admin/ventastotales" element={<VentasTotales />} />
           {/*autenticado y administrador*/}
-          <Route path="/admin/productlist" element={< ProductList />} />
+          <Route path="/admin/productlist" element={<ProductList />} />
           {/*autenticado y administrador*/}
-          <Route path="/admin/ordersList" element={< OrdersList />} />
+          <Route path="/admin/ordersList" element={<OrdersList />} />
           {/*autenticado y administrador*/}
-          <Route path="/admin/reviewslist" element={< ReviewsList />} />
+          <Route path="/admin/reviewslist" element={<ReviewsList />} />
           {/*autenticado y administrador*/}
         </Route>
 
         <Route path="/account/register" element={<Register />} />
         {/*sin logear*/}
-
-
       </Routes>
       <Footer />
     </>
--- cell-store/src/components/menuAccount/MenuAccount.jsx
+++ cell-store/src/components/menuAccount/MenuAccount.jsx
@@ -1,24 +1,24 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Avatar from '@mui/material/Avatar';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Tooltip from '@mui/material/Tooltip';
-import PersonAdd from '@mui/icons-material/PersonAdd';
-import Settings from '@mui/icons-material/Settings';
-import Logout from '@mui/icons-material/Logout';
-import { useDispatch, useSelector } from 'react-redux';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import styles from "./MenuAccount.module.css"
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Avatar from "@mui/material/Avatar";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Tooltip from "@mui/material/Tooltip";
+import PersonAdd from "@mui/icons-material/PersonAdd";
+import Settings from "@mui/icons-material/Settings";
+import Logout from "@mui/icons-material/Logout";
+import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import styles from "./MenuAccount.module.css";
 const MenuAccount = () => {
-    const user_redux = useSelector((state) => state.user.user);
+  const user_redux = useSelector((state) => state.user.user);
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
@@ -37,19 +37,21 @@ const MenuAccount = () => {
   };
   return (
     <React.Fragment>
-      <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
-       
+      <Box sx={{ display: "flex", alignItems: "center", textAlign: "center" }}>
         <Tooltip title="Account settings">
           <IconButton
             onClick={handleClick}
             size="small"
             sx={{ ml: 2 }}
-            aria-controls={open ? 'account-menu' : undefined}
+            aria-controls={open ? "account-menu" : undefined}
             aria-haspopup="true"
-            aria-expanded={open ? 'true' : undefined}
+            aria-expanded={open ? "true" : undefined}
           >
-            <Avatar sx={{ width: 40, height: 40 }}>{Object.keys(user_redux).length > 0 ?  user_redux?.name?.charAt().toUpperCase()
-                    : user?.name.charAt().toUpperCase()}</Avatar>
+            <Avatar sx={{ width: 40, height: 40 }}>
+              {Object.keys(user_redux).length > 0
+                ? user_redux?.name?.charAt().toUpperCase()
+                : user?.name.charAt().toUpperCase()}
+            </Avatar>
           </IconButton>
         </Tooltip>
       </Box>
@@ -62,68 +64,71 @@ const MenuAccount = () => {
         PaperProps={{
           elevation: 0,
           sx: {
-            overflow: 'visible',
-            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            overflow: "visible",
+            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
             mt: 1.5,
-            '& .MuiAvatar-root': {
+            "& .MuiAvatar-root": {
               width: 32,
               height: 32,
               ml: -0.5,
               mr: 1,
             },
-            '&:before': {
+            "&:before": {
               content: '""',
-              display: 'block',
-              position: 'absolute',
+              display: "block",
+              position: "absolute",
               top: 0,
               right: 14,
               width: 10,
               height: 10,
-              bgcolor: 'background.paper',
-              transform: 'translateY(-50%) rotate(45deg)',
+              bgcolor: "background.paper",
+              transform: "translateY(-50%) rotate(45deg)",
               zIndex: 0,
             },
           },
         }}
-        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
       >
         <MenuItem>
           <Avatar /> Profile
         </MenuItem>
-        <Link to='/account/profile' className={styles.link}>
-        <MenuItem>
-          <Avatar /> My account
-        </MenuItem>
+        <Link to="/account/profile" className={styles.link}>
+          <MenuItem>
+            <Avatar /> My account
+          </MenuItem>
         </Link>
         <Divider />
-        {user_redux?.admin === true || user?.admin === true ?
-        <Link className={styles.link} to='/admin'>
-        <MenuItem>
-          <ListItemIcon>
-            <Settings fontSize="small" />
-          </ListItemIcon>
-          DashBoard Admin
-        </MenuItem>
-        </Link>
-        
-        : null}
+        {user_redux?.admin === true || user?.admin === true ? (
+          <Link className={styles.link} to="/admin">
+            <MenuItem>
+              <ListItemIcon>
+                <Settings fontSize="small" />
+              </ListItemIcon>
+              DashBoard Admin
+            </MenuItem>
+          </Link>
+        ) : null}
         <MenuItem>
           <ListItemIcon>
             <Settings fontSize="small" />
           </ListItemIcon>
           Settings
+          <Link
+            className={styles.link}
+            to={`/account/profile/${user_redux._id}/usersettings`}
+          ></Link>
         </MenuItem>
         <Link onClick={handleLogOut} className={styles.link}>
-        <MenuItem>
-          <ListItemIcon>
-            <Logout fontSize="small" />
-          </ListItemIcon>
-          Logout
-        </MenuItem>
+          <MenuItem>
+            <ListItemIcon>
+              <Logout fontSize="small" />
+            </ListItemIcon>
+            Logout
+          </MenuItem>
         </Link>
       </Menu>
     </React.Fragment>
   );
-    }
-export default MenuAccount
+};
+export default MenuAccount;
