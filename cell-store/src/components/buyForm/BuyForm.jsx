c18733d7756f8465443514cfd1e17cd239fef22d
Chaos <antonioscutari2@hotmail.com>
2022-10-19 14:38:58 -0300

    Errores de merge

--- cell-store/src/components/buyForm/BuyForm.jsx
+++ cell-store/src/components/buyForm/BuyForm.jsx
@@ -1,182 +1,187 @@
-<<<<<<< HEAD
-import React, { useState, forwardRef } from 'react'
-import style from './BuyForm.module.css'
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import { Alert } from '@mui/material';
-import AlertTitle from '@mui/material/AlertTitle';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import Snackbar from '@mui/material/Snackbar';
-import MuiAlert from '@mui/material/Alert';
-import { useDispatch } from 'react-redux';
-import { orderProduct } from '../../redux/actions/productActions';
-import { errorInputForm } from './controlForm';
+import React, { useState, forwardRef } from "react";
+import style from "./BuyForm.module.css";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { Alert } from "@mui/material";
+import AlertTitle from "@mui/material/AlertTitle";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+import { useDispatch } from "react-redux";
+import { orderProduct } from "../../redux/actions/productActions";
+import { errorInputForm } from "./controlForm";
 
-
-
- 
-
-
-const BuyForm = ({error, location}) => {
-  const dispatch = useDispatch()
+const BuyForm = ({ error, location }) => {
+  const dispatch = useDispatch();
   const [open, setOpen] = React.useState(false);
-  const [errorf , setError] = useState({})
+  const [errorf, setError] = useState({});
   const [input, setInput] = useState({
-    name: '',
-    lastName: '',
-    email : '',
-    phone: '',
-    info : '',
-    address_user: ''
-  })
+    name: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    info: "",
+    address_user: "",
+  });
 
   const handleClick = () => {
     setOpen(true);
 
-    if (localStorage.getItem('user')) {
-			const productArray = JSON.parse(localStorage.getItem('cart'))
-			
-			const id = JSON.parse(localStorage.getItem('user'))
-			
-			dispatch(orderProduct(productArray, id._id, location, input))
-		
+    if (localStorage.getItem("user")) {
+      const productArray = JSON.parse(localStorage.getItem("cart"));
 
-	
-		} else {
-			navigate('/account/login')
-			
-		}
+      const id = JSON.parse(localStorage.getItem("user"));
+
+      dispatch(orderProduct(productArray, id._id, location, input));
+    } else {
+      navigate("/account/login");
+    }
   };
 
   const handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
+    if (reason === "clickaway") {
       return;
     }
 
     setOpen(false);
   };
-  
-
-
-
 
   const handleOnChange = (e) => {
     setInput({
       ...input,
-      [e.target.name] : e.target.value
-    })
-     
-    setError(errorInputForm({...input, [e.target.name]: e.target.value}))
-  }
-  
- 
-  console.log(input)
-  const handleOnSubmit = (e) =>{
-   e.preventDefault()
+      [e.target.name]: e.target.value,
+    });
 
-   setInput({
-    name: '',
-    lastName: '',
-    email : '',
-    phone: '',
-    info : '',
-    address_user: ''
-  })
+    setError(errorInputForm({ ...input, [e.target.name]: e.target.value }));
+  };
 
-  }
+  console.log(input);
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
 
- console.log(Object.values(input).join("").length)
+    setInput({
+      name: "",
+      lastName: "",
+      email: "",
+      phone: "",
+      info: "",
+      address_user: "",
+    });
+  };
+
+  console.log(Object.values(input).join("").length);
 
   return (
-     <>
-      {error && <Alert severity="info">
-  <AlertTitle>Info</AlertTitle>
-  Necesitas elegir la Sucursal — <strong>RETIRAR EN</strong>
-</Alert> }
-      <form className={style.form} onSubmit={handleOnSubmit}>
-
-       <div className={style.title}><h2>Informacion Personal</h2></div>
-      <TextField error={errorf.name && true} helperText={errorf.name} name='name' onChange={handleOnChange} value={input.name}  label="Nombre" variant="outlined" sx={{
-        '& > :not(style)': { m: 1, width: '25ch', height: '6ch' },
-      }} />
-
-      <TextField error={errorf.lastName && true} helperText={errorf.lastName} name='lastName' onChange={handleOnChange} value={input.lastName}  label="Apellido" variant="outlined" sx={{
-        '& > :not(style)': { m: 1, width: '25ch', height: '6ch' },
-      }} />
-
-      <TextField error={errorf.email&& true} helperText={errorf.email} name='email' onChange={handleOnChange} value={input.email}  label="Email" variant="outlined" sx={{
-        '& > :not(style)': { m: 1, width: '50ch', height: '6ch' },
-      }} />
-            
-      <TextField error={errorf.phone && true} helperText={errorf.phone} name='phone' onChange={handleOnChange} value={input.phone}  label="Celular" variant="outlined" sx={{
-        '& > :not(style)': { m: 1, width: '50ch', height: '6ch' },
-      }} />
-
-       <TextField  name='info' onChange={handleOnChange} value={input.info}  label="Informacion adicional" variant="outlined" sx={{
-        '& > :not(style)': { m: 1, width: '50ch', height: '15ch' },
-      }} /> 
-
-    <TextField error={errorf.address_user&& true} name='address_user' helperText={errorf.address_user} onChange={handleOnChange} value={input.address_user}  label="Domicilio" variant="outlined" sx={{
-        '& > :not(style)': { m: 1, width: '50ch', height: '6ch'},
-      }} /> 
-
-     {!error && Object.values(input).join("").length !== 0 &&  <input type='submit' disabled={ Object.keys(errorf).length !== 0 && true}  onClick={handleClick} value='Finalizar la Orden'></input>
-       }
-   
-
-      </form>
-      { <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
-          Tu orden fue Registrada!
+    <>
+      {error && (
+        <Alert severity="info">
+          <AlertTitle>Info</AlertTitle>
+          Necesitas elegir la Sucursal — <strong>RETIRAR EN</strong>
         </Alert>
-      </Snackbar>}
-      </>
-=======
-import React from 'react'
-import style from './BuyForm.module.css'
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-const BuyForm = () => {
-  return (
-   
-    <div className={style.container}>
-      <form className={style.form}>
+      )}
+      <form className={style.form} onSubmit={handleOnSubmit}>
+        <div className={style.title}>
+          <h2>Informacion Personal</h2>
+        </div>
+        <TextField
+          error={errorf.name && true}
+          helperText={errorf.name}
+          name="name"
+          onChange={handleOnChange}
+          value={input.name}
+          label="Nombre"
+          variant="outlined"
+          sx={{
+            "& > :not(style)": { m: 1, width: "25ch", height: "6ch" },
+          }}
+        />
 
-       <div className={style.title}><h2>Informacion Personal</h2></div>
-      <TextField id="outlined-basic" label="Nombre" variant="outlined" sx={{
-        '& > :not(style)': { m: 1, width: '40ch', height: '6ch' },
-      }} />
+        <TextField
+          error={errorf.lastName && true}
+          helperText={errorf.lastName}
+          name="lastName"
+          onChange={handleOnChange}
+          value={input.lastName}
+          label="Apellido"
+          variant="outlined"
+          sx={{
+            "& > :not(style)": { m: 1, width: "25ch", height: "6ch" },
+          }}
+        />
 
-      <TextField id="outlined-basic" label="Apellido" variant="outlined" sx={{
-        '& > :not(style)': { m: 1, width: '40ch', height: '6ch' },
-      }} />
+        <TextField
+          error={errorf.email && true}
+          helperText={errorf.email}
+          name="email"
+          onChange={handleOnChange}
+          value={input.email}
+          label="Email"
+          variant="outlined"
+          sx={{
+            "& > :not(style)": { m: 1, width: "50ch", height: "6ch" },
+          }}
+        />
 
-      <TextField id="outlined-basic" label="Email" variant="outlined" sx={{
-        '& > :not(style)': { m: 1, width: '80ch', height: '6ch' },
-      }} />
-            
-      <TextField id="outlined-basic" label="Celular" variant="outlined" sx={{
-        '& > :not(style)': { m: 1, width: '80ch', height: '6ch' },
-      }} />
+        <TextField
+          error={errorf.phone && true}
+          helperText={errorf.phone}
+          name="phone"
+          onChange={handleOnChange}
+          value={input.phone}
+          label="Celular"
+          variant="outlined"
+          sx={{
+            "& > :not(style)": { m: 1, width: "50ch", height: "6ch" },
+          }}
+        />
 
-       <TextField id="outlined-basic" label="Informacion adicional" variant="outlined" sx={{
-        '& > :not(style)': { m: 1, width: '80ch', height: '15ch' },
-      }} /> 
+        <TextField
+          name="info"
+          onChange={handleOnChange}
+          value={input.info}
+          label="Informacion adicional"
+          variant="outlined"
+          sx={{
+            "& > :not(style)": { m: 1, width: "50ch", height: "15ch" },
+          }}
+        />
 
-    <TextField id="outlined-basic" label="Domicilio" variant="outlined" sx={{
-        '& > :not(style)': { m: 1, width: '80ch', height: '6ch'},
-      }} /> 
+        <TextField
+          error={errorf.address_user && true}
+          name="address_user"
+          helperText={errorf.address_user}
+          onChange={handleOnChange}
+          value={input.address_user}
+          label="Domicilio"
+          variant="outlined"
+          sx={{
+            "& > :not(style)": { m: 1, width: "50ch", height: "6ch" },
+          }}
+        />
 
-      <input type='submit' value='Finalizar la Orden'></input>
-      
-   
-
+        {!error && Object.values(input).join("").length !== 0 && (
+          <input
+            type="submit"
+            disabled={Object.keys(errorf).length !== 0 && true}
+            onClick={handleClick}
+            value="Finalizar la Orden"
+          ></input>
+        )}
       </form>
-      </div>
->>>>>>> main
-  
-  )
-}
+      {
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+          <Alert
+            onClose={handleClose}
+            severity="success"
+            sx={{ width: "100%" }}
+          >
+            Tu orden fue Registrada!
+          </Alert>
+        </Snackbar>
+      }
+    </>
+  );
+};
 
-export default BuyForm
+export default BuyForm;
